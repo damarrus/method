13605451ab99ebf06ac7ab66245e7a76020f5e11
damarrus <damarrus92@gmail.com>
2018-10-31 13:47:38 +0300

    fix delete

--- handlers/delete_manual.php
+++ handlers/delete_manual.php
@@ -4,6 +4,12 @@ include ("../config/db.php");
 
 $manual_id = $_REQUEST['manual_id'];
 
+$query = "DELETE FROM blocks WHERE section_id IN (SELECT section_id FROM sections WHERE manual_id=$manual_id);";
+mysqli_query($link, $query);
+
+$query = "DELETE FROM sections WHERE manual_id=$manual_id";
+mysqli_query($link, $query);
+
 $query = "DELETE FROM manuals WHERE manual_id=$manual_id";
 mysqli_query($link, $query);
 
